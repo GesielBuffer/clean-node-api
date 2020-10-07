9e561cf939473cf34f226733b4aa44f53399b903
GesielBuffer <gesielbuffer@gmail.com>
2020-10-07 19:29:58 -0300

    move sut creation to a factory helper method

--- src/presentation/utils/email-validator.spec.js
+++ src/presentation/utils/email-validator.spec.js
@@ -6,16 +6,20 @@ class EmailValidator {
   }
 }
 
+const makesut = () => {
+  return new EmailValidator()
+}
+
 describe('Email Validator', () => {
   test('should return true if validator returns true ', () => {
-    const sut = new EmailValidator()
+    const sut = makesut()
     const isEmailValid = sut.isValid('valid_email@mail.com')
     expect(isEmailValid).toBe(true)
   })
 
   test('should return false if validator returns false ', () => {
     validator.isEmailValid = false
-    const sut = new EmailValidator()
+    const sut = makesut()
     const isEmailValid = sut.isValid('invalid_email@mail.com')
     expect(isEmailValid).toBe(false)
   })
