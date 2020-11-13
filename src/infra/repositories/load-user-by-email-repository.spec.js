d91e46212c56c4fd87f1f47b0e53eff9016a6c32
GesielBuffer <gesielbuffer@gmail.com>
2020-11-13 03:30:52 -0300

    ensure LoadUserByEmailRepository throws if no userModel is provided

--- src/infra/repositories/load-user-by-email-repository.spec.js
+++ src/infra/repositories/load-user-by-email-repository.spec.js
@@ -46,3 +46,9 @@ describe('LoadUserByEmail Repository', () => {
     })
   })
 })
+
+test('Should throw if no userModel is provided', async () => {
+  const sut = new LoadUserByEmailRepository()
+  const promise = sut.load('any_email@mail.com')
+  expect(promise).rejects.toThrow()
+})
